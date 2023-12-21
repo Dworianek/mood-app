d4373342b02df979bd8c2614ef0ef3017ddd6f7a
Adrian Dworak <adrian.dworak97@gmail.com>
2023-12-21 20:02:22 +0100

    Updates

---
+++ precache-manifest.86b0d704eb0431080bb0af84cdaa80ac.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4337e89f00c7197bd0cc90ac02bc46",
+    "url": "/mood-app/index.html"
+  },
+  {
+    "revision": "141e6113cdb141ee6aad",
+    "url": "/mood-app/static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "976d1f793ee331c2b9c0",
+    "url": "/mood-app/static/css/main.5a35bcf2.chunk.css"
+  },
+  {
+    "revision": "141e6113cdb141ee6aad",
+    "url": "/mood-app/static/js/2.58da70ea.chunk.js"
+  },
+  {
+    "revision": "976d1f793ee331c2b9c0",
+    "url": "/mood-app/static/js/main.51c5bcf7.chunk.js"
+  },
+  {
+    "revision": "97bcaa49ae0b60fbcbb1",
+    "url": "/mood-app/static/js/runtime~main.f0012241.js"
+  },
+  {
+    "revision": "d59158caa76048c8a52d5c55a0b80de1",
+    "url": "/mood-app/static/media/1.d59158ca.png"
+  },
+  {
+    "revision": "8efddaa6f86ca0de620a01e77f1df50c",
+    "url": "/mood-app/static/media/2.8efddaa6.png"
+  },
+  {
+    "revision": "0bf7b9a7fa037ade2614fd05a1639533",
+    "url": "/mood-app/static/media/3.0bf7b9a7.png"
+  },
+  {
+    "revision": "d77fb1d56c673851f15a4e71c3fc90b7",
+    "url": "/mood-app/static/media/4.d77fb1d5.png"
+  },
+  {
+    "revision": "5d662401bc2206811ab06d1eaa31dd7d",
+    "url": "/mood-app/static/media/5.5d662401.png"
+  },
+  {
+    "revision": "f412e214343cbe64145c10245bf1d285",
+    "url": "/mood-app/static/media/alarm.f412e214.png"
+  },
+  {
+    "revision": "be52964e18c4408dd12e22669430b546",
+    "url": "/mood-app/static/media/breakfast.be52964e.png"
+  },
+  {
+    "revision": "9f6b664219014a1cad0d39c19f30d2dc",
+    "url": "/mood-app/static/media/cleanup.9f6b6642.png"
+  },
+  {
+    "revision": "05c0abad59294ae38064d98546036d7a",
+    "url": "/mood-app/static/media/dance.05c0abad.png"
+  },
+  {
+    "revision": "08f445e299bff3b7242f0d4cbaed49f2",
+    "url": "/mood-app/static/media/dinner.08f445e2.png"
+  },
+  {
+    "revision": "8903b866c90866ba97d37a6f19329747",
+    "url": "/mood-app/static/media/gaming.8903b866.png"
+  },
+  {
+    "revision": "f038279f7fcd93e127dd0cae90adcf1b",
+    "url": "/mood-app/static/media/ironing.f038279f.png"
+  },
+  {
+    "revision": "8ac1c6e538a7733a1246fde03cc556e4",
+    "url": "/mood-app/static/media/listen.8ac1c6e5.png"
+  },
+  {
+    "revision": "09bef442095c25281e61c0d7c5e3e91e",
+    "url": "/mood-app/static/media/party.09bef442.png"
+  },
+  {
+    "revision": "f456ce5a460194c37ad92f27b46d6009",
+    "url": "/mood-app/static/media/read.f456ce5a.png"
+  },
+  {
+    "revision": "a1a772bfe007829381b295927971706e",
+    "url": "/mood-app/static/media/shopping.a1a772bf.png"
+  },
+  {
+    "revision": "4281df93e57e91b095cd32549d24482e",
+    "url": "/mood-app/static/media/shower.4281df93.png"
+  },
+  {
+    "revision": "7b7f2b516ad11c878694859aba6a2466",
+    "url": "/mood-app/static/media/sleep.7b7f2b51.png"
+  },
+  {
+    "revision": "2644abc66b583ab6f1a717e1117d90ad",
+    "url": "/mood-app/static/media/sports.2644abc6.png"
+  },
+  {
+    "revision": "287fe2b8f8091472e0d7434f294f51f4",
+    "url": "/mood-app/static/media/study.287fe2b8.png"
+  },
+  {
+    "revision": "91fa0176dd477eaf037e18d99c75492b",
+    "url": "/mood-app/static/media/tv.91fa0176.png"
+  },
+  {
+    "revision": "480a9e37a3fd78634a4ea0db67b72dc8",
+    "url": "/mood-app/static/media/walk.480a9e37.png"
+  },
+  {
+    "revision": "834c7fd75d1a2d8562a2fad5606261f4",
+    "url": "/mood-app/static/media/wash.834c7fd7.png"
+  }
+]);
